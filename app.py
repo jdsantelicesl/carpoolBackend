5a0255850b3b773c7235e1900f1236090a55b1e8
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-08-04 22:10:24 -0700

    minor changes

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 from flask import Flask
 from pymongo import MongoClient
-from blueprints.ride import ride_bp
+from blueprints.ride_bp import ride_bp
+from blueprints.user_bp import user_bp
 
 app = Flask(__name__)
 
@@ -17,17 +18,11 @@ app.config["MONGO_CLIENT"] = client
 # this imports all of the routes in blueprints/ride.py
 # the routes will be accesses via "/ride/" + route
 app.register_blueprint(ride_bp, url_prefix="/ride")
+app.register_blueprint(user_bp, url_prefix="/user")
 
 @app.route("/")
 def index():
-    try:
-        database = client.get_database("carpool")
-        users = database.get_collection("users")
-        # Query for a movie that has the title 'Back to the Future'
-        users.insert_one({"name": "new User"})
-        return "success"
-    except:
-        return "error"
+    return "welcome"
 
 if __name__ == "__main__":
     # this allows for app to run on local wifi, port 5000, and will reflect changes upon save
